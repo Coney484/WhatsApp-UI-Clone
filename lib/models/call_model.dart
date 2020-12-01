68b34e2ecf433b8b9a097ba72ab058524d1f5766
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-12-01 11:08:40 +0530

    worked on dummy model data

---
+++ lib/models/call_model.dart
@@ -0,0 +1,39 @@
+class Calls {
+  final String name;
+  final String time;
+  final String imageUrl;
+
+  Calls({
+    this.name,
+    this.time,
+    this.imageUrl,
+  });
+}
+
+List<Calls> callData = [
+  new Calls(
+      name: "Alex Maxwell",
+      time: "30 November, 7:03 pm",
+      imageUrl:
+          "https://i.pinimg.com/236x/4c/f6/70/4cf6700dd100334c0b4b857ce77fc5d9.jpg"),
+  new Calls(
+      name: "Alex Maxwell",
+      time: "30 November, 7:03 pm",
+      imageUrl:
+          "https://i.pinimg.com/236x/4c/f6/70/4cf6700dd100334c0b4b857ce77fc5d9.jpg"),
+  new Calls(
+      name: "Alex Maxwell",
+      time: "30 November, 7:03 pm",
+      imageUrl:
+          "https://i.pinimg.com/236x/4c/f6/70/4cf6700dd100334c0b4b857ce77fc5d9.jpg"),
+  new Calls(
+      name: "Alex Maxwell",
+      time: "30 November, 7:03 pm",
+      imageUrl:
+          "https://i.pinimg.com/236x/4c/f6/70/4cf6700dd100334c0b4b857ce77fc5d9.jpg"),
+  new Calls(
+      name: "Alex Maxwell",
+      time: "30 November, 7:03 pm",
+      imageUrl:
+          "https://i.pinimg.com/236x/4c/f6/70/4cf6700dd100334c0b4b857ce77fc5d9.jpg"),
+];
---
+++ lib/models/chat_model.dart
@@ -0,0 +1,71 @@
+class Chat {
+  final String name;
+  String message;
+  final String time;
+  final String imageUrl;
+
+  Chat({
+    this.name,
+    this.message,
+    this.time,
+    this.imageUrl,
+  });
+}
+
+List<Chat> messageData = [
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/VVq6KcT.png" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/6wKo3Y6.png" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/6wKo3Y6.png" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/VVq6KcT.png" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/kW4fzuib.jpg" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/kW4fzuib.jpg" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/kW4fzuib.jpg" ),
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/kW4fzuib.jpg" ),            
+
+  new Chat(
+      name: "Coney Codes",
+      message: "I love Flutter",
+      time: "10:12",
+      imageUrl: "https://i.imgur.com/VVq6KcT.png" ),    
+
+  
+];
